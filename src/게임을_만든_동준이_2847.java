7ce1d0428b4002e3ec8776c6fb6674c23a31cb5c
gyurim <gyurim1250@gmail.com>
2025-01-19 20:28:27 +0900

    250116

--- src/게임을_만든_동준이_2847.java
+++ src/게임을_만든_동준이_2847.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.*;
 
+/*******************************************************************************
+ * 소요시간: 1시간
+ * 시간복잡도: O(n)
+ * 메모리: 14252 kb
+ * 시간: 108 ms
+ *******************************************************************************/
 public class 게임을_만든_동준이_2847 {
 
     public static void main(String[] args) throws IOException {
@@ -8,20 +14,17 @@ public class 게임을_만든_동준이_2847 {
         int n = Integer.parseInt(br.readLine());
         int[] grades = new int[n];
 
-        int maxNum = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             grades[i] = Integer.parseInt(br.readLine());
-            maxNum = Math.max(maxNum, grades[i]);
         }
 
-        int minCount = Integer.MAX_VALUE;
-        for (int start = 1; start <= maxNum; start++) { // O(20000)
-            int count = 0;
-            int prevNum = grades[0];
-
-            for (int i = 0; i < n; i++) {
-
+        int count = 0;
+        for (int i = n - 2; i >= 0; i--) { // O(n)
+            if (grades[i] >= grades[i + 1]) { // 현재 레벨이 다음 레벨보다 크다면
+                count += (grades[i] - (grades[i + 1] - 1));
+                grades[i] = grades[i + 1] - 1; // 점수 줄이기
             }
         }
+        System.out.println(count);
     }
 }
